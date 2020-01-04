5bd1e8df6cb21e11ae11a675494b974a459c76f9
Victor Tran <victor.n.tran@outlook.com>
2020-01-04 01:03:36 -0500

    add router to app

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
 
+const {router: carRouter} = require('./routes/carsRouter');
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
 
+app.use('/api/cars', carRouter);
 
+module.exports = app;
