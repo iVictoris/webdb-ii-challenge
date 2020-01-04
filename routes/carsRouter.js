ccfac8164d641117b57745ac30da928b66622063
Victor Tran <victor.n.tran@outlook.com>
2020-01-04 00:57:51 -0500

    create middleware to validate body minimalistically and make post route

--- routes/carsRouter.js
+++ routes/carsRouter.js
@@ -8,6 +8,31 @@ const {getAll, create} = require('../data/cars');
  *  Write endpoints to support CREATE and READ operations on the cars resource.
  */
 
+const validateCar = (req, res, next) => {
+  const {body} = req;
+  const {vin, make, model, mileage} = body;
+
+  if (!body) {
+    res
+      .status(400)
+      .json({
+      message: "No data sent at all. Please send required data."
+    });
+  }
+
+  if (!vin || !make || !model || mileage) {
+    res
+      .status(400)
+      .json({
+      message: "VIN, Make, Model, and Mileage are all required. Please supply this information."
+    });
+  }
+
+  next();
+}
+
+router.post('/', validateCar);
+
 router
   .route('/')
   .get(async (req, res) => {
@@ -27,6 +52,21 @@ router
   })
   .post(async (req, res) => {
     /* POST  */
+    const {make, model, vin, mileage} = req.body;
+    const carFromBody = {make, model, vin, mileage}
+
+    try {
+      const car = await create(carFromBody);
+      res
+        .status(201)
+        .json(car);
+    } catch (error) {
+      res
+        .status(500)
+        .json({
+        message: "Server issue in creating car. Try again."
+      });
+    }
   });
 
 
